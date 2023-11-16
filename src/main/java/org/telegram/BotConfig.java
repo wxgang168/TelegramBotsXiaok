3ce40bdd5d5883801f8adbe229f7cdafdc6242ab
wangxingang <282182901@qq.com>
2023-11-16 13:05:31 +0800

    just commod bot token

--- src/main/java/org/telegram/BotConfig.java
+++ src/main/java/org/telegram/BotConfig.java
@@ -7,30 +7,30 @@ package org.telegram;
  * @date 20 of June of 2015
  */
 public class BotConfig {
-    public static final String WEATHER_TOKEN = "6950305437:AAEPZ6oXz0naOVXnRPndcXpu_y72isknuVc";
+    public static final String WEATHER_TOKEN = "";
     public static final String WEATHER_USER = "weatherbot";
 
-    public static final String TRANSIFEX_TOKEN = "6950305437:AAEPZ6oXz0naOVXnRPndcXpu_y72isknuVc";
+    public static final String TRANSIFEX_TOKEN = "";
     public static final String TRANSIFEX_USER = "TGlanguagesbot";
 
-    public static final String FILES_TOKEN = "6950305437:AAEPZ6oXz0naOVXnRPndcXpu_y72isknuVc";
+    public static final String FILES_TOKEN = "";
     public static final String FILES_USER = "filesbot";
 
-    public static final String DIRECTIONS_TOKEN = "6950305437:AAEPZ6oXz0naOVXnRPndcXpu_y72isknuVc";
+    public static final String DIRECTIONS_TOKEN = "";
     public static final String DIRECTIONS_USER = "directionsbot";
 
-    public static final String CHANNEL_TOKEN = "6950305437:AAEPZ6oXz0naOVXnRPndcXpu_y72isknuVc";
+    public static final String CHANNEL_TOKEN = "";
     public static final String CHANNEL_USER = "channelupdatesbot";
 
-    public static final String RAE_TOKEN = "6950305437:AAEPZ6oXz0naOVXnRPndcXpu_y72isknuVc";
+    public static final String RAE_TOKEN = "";
     public static final String RAE_USER = "raebot";
 
-    public static final String WEBHOOK_TOKEN = "6950305437:AAEPZ6oXz0naOVXnRPndcXpu_y72isknuVc";
+    public static final String WEBHOOK_TOKEN = "";
     public static final String WEBHOOK_USER = "webhooksamplebot";
 
-    public static final String COMMANDS_TOKEN = "6950305437:AAEPZ6oXz0naOVXnRPndcXpu_y72isknuVc";
+    public static final String COMMANDS_TOKEN = "6950305437:AAG-pfc0taK7W7ynXt66JgaT43PJ1boV2KE";
     public static final String COMMANDS_USER = "MyCommandsBot";
     
-    public static final String ELEKTROLLART_TOKEN = "6950305437:AAEPZ6oXz0naOVXnRPndcXpu_y72isknuVc";
+    public static final String ELEKTROLLART_TOKEN = "";
     public static final String ELEKTROLLART_USER = "ElektrollArtFanBot";
 }
