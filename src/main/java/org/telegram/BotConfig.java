9cdb41cb1dc8ae71a0670f4dccb3c7977d2d4a07
wangxingang <282182901@qq.com>
2023-11-16 11:10:32 +0800

    add  xiaok168  config

--- src/main/java/org/telegram/BotConfig.java
+++ src/main/java/org/telegram/BotConfig.java
@@ -7,30 +7,30 @@ package org.telegram;
  * @date 20 of June of 2015
  */
 public class BotConfig {
-    public static final String WEATHER_TOKEN = "<token>";
+    public static final String WEATHER_TOKEN = "6950305437:AAEPZ6oXz0naOVXnRPndcXpu_y72isknuVc";
     public static final String WEATHER_USER = "weatherbot";
 
-    public static final String TRANSIFEX_TOKEN = "<token>";
+    public static final String TRANSIFEX_TOKEN = "6950305437:AAEPZ6oXz0naOVXnRPndcXpu_y72isknuVc";
     public static final String TRANSIFEX_USER = "TGlanguagesbot";
 
-    public static final String FILES_TOKEN = "<token>";
+    public static final String FILES_TOKEN = "6950305437:AAEPZ6oXz0naOVXnRPndcXpu_y72isknuVc";
     public static final String FILES_USER = "filesbot";
 
-    public static final String DIRECTIONS_TOKEN = "<token>";
+    public static final String DIRECTIONS_TOKEN = "6950305437:AAEPZ6oXz0naOVXnRPndcXpu_y72isknuVc";
     public static final String DIRECTIONS_USER = "directionsbot";
 
-    public static final String CHANNEL_TOKEN = "<token>";
+    public static final String CHANNEL_TOKEN = "6950305437:AAEPZ6oXz0naOVXnRPndcXpu_y72isknuVc";
     public static final String CHANNEL_USER = "channelupdatesbot";
 
-    public static final String RAE_TOKEN = "<token>";
+    public static final String RAE_TOKEN = "6950305437:AAEPZ6oXz0naOVXnRPndcXpu_y72isknuVc";
     public static final String RAE_USER = "raebot";
 
-    public static final String WEBHOOK_TOKEN = "<token>";
+    public static final String WEBHOOK_TOKEN = "6950305437:AAEPZ6oXz0naOVXnRPndcXpu_y72isknuVc";
     public static final String WEBHOOK_USER = "webhooksamplebot";
 
-    public static final String COMMANDS_TOKEN = "<token>";
+    public static final String COMMANDS_TOKEN = "6950305437:AAEPZ6oXz0naOVXnRPndcXpu_y72isknuVc";
     public static final String COMMANDS_USER = "MyCommandsBot";
     
-    public static final String ELEKTROLLART_TOKEN = "<token>";
+    public static final String ELEKTROLLART_TOKEN = "6950305437:AAEPZ6oXz0naOVXnRPndcXpu_y72isknuVc";
     public static final String ELEKTROLLART_USER = "ElektrollArtFanBot";
 }
--- src/main/java/org/telegram/BuildVars.java
+++ src/main/java/org/telegram/BuildVars.java
@@ -15,7 +15,7 @@ public class BuildVars {
     public static final Boolean useWebHook = false;
     public static final int PORT = 8443;
     public static final String EXTERNALWEBHOOKURL = "https://example.changeme.com:" + PORT; // https://(xyz.)externaldomain.tld
-    public static final String INTERNALWEBHOOKURL = "https://localhost.changeme.com:" + PORT; // https://(xyz.)localip/domain(.tld)
+    public static final String INTERNALWEBHOOKURL = "http://localhost:" + PORT; // https://(xyz.)localip/domain(.tld)
     public static final String pathToCertificatePublicKey = "./YOURPEM.pem"; //only for self-signed webhooks
     public static final String pathToCertificateStore = "./YOURSTORE.jks"; //self-signed and non-self-signed.
     public static final String certificateStorePassword = "yourpass"; //password for your certificate-store
@@ -30,10 +30,10 @@ public class BuildVars {
 
     public static final String pathToLogs = "./";
 
-    public static final String linkDB = "jdbc:mysql://localhost:3306/YOURDATABSENAME?useUnicode=true&characterEncoding=UTF-8";
+    public static final String linkDB = "jdbc:mysql://localhost:3306/xiaok168db?useUnicode=true&characterEncoding=UTF-8";
     public static final String controllerDB = "com.mysql.cj.jdbc.Driver";
-    public static final String userDB = "<your-database-user>";
-    public static final String password = "<your-databas-user-password>";
+    public static final String userDB = "root";
+    public static final String password = "1qaz2wsx";
 
     static {
         // Add elements to ADMIN array here
